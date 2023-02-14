69dd27b64b1658b313807151a68b735f42e11027
UrvikPiparva <121550018+UrvikPiparva@users.noreply.github.com>
2023-02-14 11:22:29 +0530

    changecode

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -7,53 +7,58 @@ class FirstPage extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
 
-    return Scaffold(
-      body: Column(
-        children: [
-          Center(
-              child: Image.asset(
-                'assets/images/chef page 1.png',
-                height: 300,
-                width: 300,
-              )),
-          Text(
-            "The Fastesst \nFood Delivery",
-            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 20),
-            child: Text(
-              'Lorem ipsum dolor sit amet, consetetur \nsedipscing elir, sed diam nonumy',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 50),
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => SecondPage()),
-                );
-              },
-              child: Padding(
-                padding: const EdgeInsets.only(
-                    left: 20, right: 20, top: 10, bottom: 10),
-                child: Text('Get Started',
-                    style: TextStyle(color: Colors.white, fontSize: 15)),
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          padding: EdgeInsets.only(top: 80),
+          child: Column(
+            children: [
+              Center(
+                  child: Image.asset(
+                    'assets/images/chef page 1.png',
+                    height: 300,
+                    width: 300,
+                  )),
+              Text(
+                "The Fastesst \nFood Delivery",
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
               ),
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.brown),
-                shape: MaterialStateProperty.all(
-                  RoundedRectangleBorder(
-                    // Change your radius here
-                    borderRadius: BorderRadius.circular(20),
-                  ),
+              Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: Text(
+                  'Lorem ipsum dolor sit amet, consetetur \nsedipscing elir, sed diam nonumy',
+                  style: TextStyle(fontWeight: FontWeight.bold),
                 ),
               ),
-            ),
-          )
-        ],
+              Padding(
+                padding: const EdgeInsets.only(top: 50),
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => SecondPage()),
+                    );
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        left: 20, right: 20, top: 10, bottom: 10),
+                    child: Text('Get Started',
+                        style: TextStyle(color: Colors.white, fontSize: 15)),
+                  ),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.brown),
+                    shape: MaterialStateProperty.all(
+                      RoundedRectangleBorder(
+                        // Change your radius here
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
 
